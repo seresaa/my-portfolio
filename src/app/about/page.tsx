f1545629aea37aa62ceb2cc1fb30560dd041087e
seresaa <cherry.mae.cantones.0813@gmail.com>
2024-06-30 18:44:13 +0800

    +add new projects

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,105 +4,106 @@ import MyImage from "@/images/me4.jpg";
 import TabButton from "../components/tabButton";
 import React, { useTransition, useState } from "react";
 
-
 const TAB_DATA = [
-    {
-      title: "Skills",
-      id: "skills",
-      content: (
-        <ul className="list-disc pl-2 text-white">
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>React JS</li>
-          <li>Next JS</li>
-          <li>Typescript</li>
-          <li>Flutter</li>
-          <li>Version Control/Git</li>
-          <li>UI/UX Figma</li>
-        </ul>
-      ),
-    },
-    {
-      title: "Education",
-      id: "education",
-      content: (
-        <ul className="list-disc pl-2 text-white">
-          <li>University of Makati, BSCS major in Application Development</li>
-          <li>University of Makati, Diploma in Application Development</li>
-          <li>University of Makati, Senior High School ICT</li>
-        </ul>
-      ),
-    },
-    {
-      title: "Certifications",
-      id: "certifications",
-      content: (
-        <ul className="list-disc pl-2 text-white">
-          <li>Comptia ITF+ Certified</li>
-        </ul>
-      ),
-    },
-  ];
-  
-  export default function About() {
-    const [tab, setTab] = useState("skills");
-  
-    const handleTabChange = (id: string) => {
-      setTab(id);
-    };
-  
-    return (
-      <div className="w-full h-screen">
-        <div className="mt-10 ">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
-            <div className="md:col-span-1 flex flex-col items-center mx-4">
-              <Image
-                src={MyImage}
-                alt={"MyImage"}
-                className="m-h-[50] w-[30rem] rounded-lg"
-              />
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2 text-white">
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>React JS</li>
+        <li>Next JS</li>
+        <li>Typescript</li>
+        <li>Flutter</li>
+        <li>Version Control/Git</li>
+        <li>UI/UX Figma</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2 text-white">
+        <li>University of Makati, BSCS major in Application Development</li>
+        <li>University of Makati, Diploma in Application Development</li>
+        <li>University of Makati, Senior High School ICT</li>
+      </ul>
+    ),
+  },
+
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2 text-white">
+        <li>Comptia ITF+ Certified</li>
+      </ul>
+    ),
+  },
+];
+
+export default function About() {
+  const [tab, setTab] = useState("skills");
+
+  const handleTabChange = (id: string) => {
+    setTab(id);
+  };
+
+  return (
+    <div className="w-full h-screen">
+      <div className="mt-10 ">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
+          <div className="md:col-span-1 flex flex-col items-center mx-4">
+            <Image
+              src={MyImage}
+              alt={"MyImage"}
+              className="m-h-[50] w-[30rem] rounded-lg"
+            />
+          </div>
+          <div className="md:col-span-1 m-4 ">
+            <h1 className="font-bold text-primary text-5xl mb-10 ">About me</h1>
+            <h3 className="text-1xl text-[#FFFF]">
+              I am a graduating student from the University of Makati, pursuing
+              a Bachelor of Science in Computer Science with a major in
+              Application Development. My academic journey has equipped me with
+              a strong foundation in software development, and I am eager to
+              transition into the software industry upon graduation. My passion
+              lies in creating innovative solutions, particularly within the
+              realm of front-end development. I
+            </h3>
+            <div className="flex flex-row justify-start mt-8">
+              <TabButton
+                selectTab={() => handleTabChange("skills")}
+                active={tab === "skills"}
+              >
+                {" "}
+                Skills{" "}
+              </TabButton>
+              <TabButton
+                selectTab={() => handleTabChange("education")}
+                active={tab === "education"}
+              >
+                {" "}
+                Education{" "}
+              </TabButton>
+              <TabButton
+                selectTab={() => handleTabChange("certifications")}
+                active={tab === "certifications"}
+              >
+                {" "}
+                Certifications{" "}
+              </TabButton>
             </div>
-            <div className="md:col-span-1 m-4 ">
-              <h1 className="font-bold text-primary text-5xl mb-10 ">About me</h1>
-              <h3 className="text-1xl text-[#FFFF]">
-             I am a graduating student from the University of Makati, pursuing a Bachelor of Science in Computer Science
-              with a major in Application Development. My academic journey has equipped me with a strong foundation in
-               software development, and I am eager to transition into the software industry upon graduation. My passion
-                lies in creating innovative solutions, particularly within the realm of front-end development. I
-              </h3>
-              <div className="flex flex-row justify-start mt-8">
-                <TabButton
-                  selectTab={() => handleTabChange("skills")}
-                  active={tab === "skills"}
-                >
-                  {" "}
-                  Skills{" "}
-                </TabButton>
-                <TabButton
-                  selectTab={() => handleTabChange("education")}
-                  active={tab === "education"}
-                >
-                  {" "}
-                  Education{" "}
-                </TabButton>
-                <TabButton
-                  selectTab={() => handleTabChange("certifications")}
-                  active={tab === "certifications"}
-                  
-                  
-                >
-                  {" "}
-                  Certifications{" "}
-                </TabButton>
-              </div>
-              <div className="mt-8">
-                {tab === "skills" && TAB_DATA[0].content}
-                {tab === "education" && TAB_DATA[1].content}
-                {tab === "certifications" && TAB_DATA[2].content}
-              </div>
+            <div className="mt-8">
+              {tab === "skills" && TAB_DATA[0].content}
+              {tab === "education" && TAB_DATA[1].content}
+              {tab === "certifications" && TAB_DATA[2].content}
             </div>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
--- src/app/projectspage/page.tsx
+++ src/app/projectspage/page.tsx
@@ -1,49 +1,67 @@
 "use client";
-import Proj1 from "@/images/proj1.png"
-import Proj2 from "@/images/Proj2.png"
-import Proj3 from "@/images/proj3.png"
-import Proj4 from "@/images/Proj4.png"
+import Proj1 from "@/images/project1.png";
+import Proj2 from "@/images/project2.png";
+import Proj3 from "@/images/project3.png";
+import Proj4 from "@/images/project4.png";
+import Proj5 from "@/images/project5.png";
 import Project from "../components/project";
-
 
 export default function ProjectsPage() {
   return (
     <div className="w-full h-screen">
-  
-
       <div className="mt-10 flex flex-col items-center">
         <h1 className="text-primary text-3xl font-bold">Personal Projects</h1>
         <h2 className="text-white">These are my recent personal projects.</h2>
 
         <div className="grid grid-cols-1 md:grid-cols2 lg:grid-cols-3">
-          <Project 
+          <Project
             projectImg={Proj1}
             title={"Personal portfolio website"}
-            description={"I created this personal website to showcase my current knowledge and skills"}
-            frameworks={"Next JS, TailwindCSS"} repoLink={"https://github.com/seresaa/my-portfolio.git"}            />
-            <Project 
+            description={
+              "I created this personal website to showcase my current knowledge and skills"
+            }
+            frameworks={"Next JS, TailwindCSS"}
+            repoLink={"https://github.com/seresaa/my-portfolio.git"}
+          />
+          <Project
             projectImg={Proj2}
-            title={"Movie App"}
-            description={"Simple Movie App using Flutter with API Integration using TMDB API "}
-            frameworks={"Flutter"} repoLink={"https://github.com/seresaa/movieappflutter.git"}            />
+            title={"MovieMax: Movie Flutter App"}
+            description={
+              "Simple Movie App using Flutter using Provider with API Integration (TMDB API)"
+            }
+            frameworks={"Flutter"}
+            repoLink={"https://github.com/seresaa/moviemax"}
+          />
 
-          <Project 
+          <Project
             projectImg={Proj3}
-            title={"Recipe App"}
-            description={"Recipe web app using React JS with API Integration using TheMealDB API "}
-            frameworks={"React js, TailwindCSS"} repoLink={"https://github.com/seresaa/Recipe_app.git"}            />
+            title={"Booklily: Book Flutter App"}
+            description={
+              "Simple Book App using Flutter using BLOC with API Integration (Google Books API) "
+            }
+            frameworks={"Flutter"}
+            repoLink={"https://github.com/seresaa/booklily"}
+          />
 
-          <Project 
+          <Project
             projectImg={Proj4}
+            title={"Recipe App"}
+            description={
+              "Recipe web app using React JS with API Integration using TheMealDB API "
+            }
+            frameworks={"React js, TailwindCSS"}
+            repoLink={"https://github.com/seresaa/Recipe_app.git"}
+          />
+
+          <Project
+            projectImg={Proj5}
             title={"Pet Care Website Design"}
             description={"Pet care website design with reusable components "}
-            frameworks={"Vite + React, TailwindCSS"} repoLink={"https://github.com/seresaa/Pet-Care.git"}            />
-
+            frameworks={"Vite + React, TailwindCSS"}
+            repoLink={"https://github.com/seresaa/Pet-Care.git"}
+          />
         </div>
-
-        
       </div>
-     
     </div>
   );
 }
